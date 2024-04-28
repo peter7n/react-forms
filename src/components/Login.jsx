05fb92fe896d8021323beec51061bcfbdb4725b7
Peter Nguyen <ptn045@ucsd.edu>
2024-04-28 15:55:43 -0700

    validate input on form submit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,26 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Login() {
-  const email = useRef();
-  const password = useRef();
+	const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
+  	const email = useRef();
+  	const password = useRef();
   
 	function handleSubmit(event) {
 		event.preventDefault();
-    const enteredEmail = email.current.value;
-    const enteredPassword = password.current.value;
-		console.log(enteredEmail, enteredPassword);
+    	const enteredEmail = email.current.value;
+    	const enteredPassword = password.current.value;
+
+		const emailIsValid = enteredEmail.includes('@');
+
+		if (!emailIsValid) {
+			setEmailIsInvalid(true);
+			return; // to prevent any code after this from executing
+		}
+
+		setEmailIsInvalid(false); // clears error message if valid input upon submit
+
+		console.log('Sending HTTP request here...');
 	}
 
 	return (
@@ -22,8 +34,9 @@ export default function Login() {
 						id="email"
 						type="email"
 						name="email"
-            ref={email}
+						ref={email}
 					/>
+					<div className='control-error'>{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
 				</div>
 
 				<div className="control no-margin">
