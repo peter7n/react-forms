1ec86c8c7f682b3a422a0f7616a7fc9462ced636
Peter Nguyen <ptn045@ucsd.edu>
2024-04-28 15:26:35 -0700

    validate input onBlur

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -5,7 +5,13 @@ export default function Login() {
 		email: '',
 		password: '',
 	});
-	const emailIsInvalid = enteredValues.email && !enteredValues.email.includes('@');
+
+	const [didEdit, setDidEdit] = useState({
+		email: false,
+		password: false
+	})
+	
+	const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
 	function handleSubmit(event) {
 		event.preventDefault();
@@ -16,6 +22,17 @@ export default function Login() {
 		setEnteredInput((prevValues) => ({
 			...prevValues,
 			[identifier]: value,
+		}));
+		setDidEdit((prevEdit) => ({
+			...prevEdit,
+			[identifier]: false
+		}))
+	}
+
+	function handleInputBlur(identifier) {
+		setDidEdit((prevEdit) => ({
+			...prevEdit,
+			[identifier]: true
 		}));
 	}
 
@@ -30,6 +47,7 @@ export default function Login() {
 						id="email"
 						type="email"
 						name="email"
+						onBlur={() => handleInputBlur('email')}
 						onChange={(event) =>
 							handleInputChange('email', event.target.value)
 						}
